00b2728f117d17a713d831398c94580259df971e
Sshawarma <sammytheshawarma@gmail.com>
2020-06-05 17:20:44 -0400

    Made faction tag look nicer

--- SMP/src/me/Sshawarma/Events/ChatColorChanger.java
+++ SMP/src/me/Sshawarma/Events/ChatColorChanger.java
@@ -22,17 +22,17 @@ public class ChatColorChanger implements Listener{
 		//player.setDisplayName(plugin.getConfig().getString(player.getDisplayName()) + player.getDisplayName() + ChatColor.GOLD);
 		//event.setMessage(plugin.getConfig().getString(player.getDisplayName()) + msg);
 		
-		String faction = "[" + config.getString("PlayerSettings." + player.getDisplayName() + ".faction") + "]";
+		String faction = "[" + config.getString("PlayerSettings." + player.getDisplayName() + ".faction") + "] ";
 		
 		//Pretty Obvious it  grabs config and changes chat message with colors based on OP level
 		if(!(player.isOp())) {
 			event.setFormat(ChatColor.DARK_GRAY + "<" +ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("PlayerSettings." + player.getDisplayName() + ".chatcolor")) + "" +ChatColor.BOLD + player.getDisplayName() + ChatColor.DARK_GRAY + ">"
-				+ ChatColor.RESET + ChatColor.DARK_GRAY + faction + ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("PlayerSettings." + player.getDisplayName() + ".chatcolor")) + msg);
+				+ ChatColor.RESET + faction + ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("PlayerSettings." + player.getDisplayName() + ".chatcolor")) + msg);
 		}
 		
 		else {
 			event.setFormat(ChatColor.GOLD + "<" + ChatColor.RED + "" + ChatColor.BOLD +  player.getDisplayName() + ChatColor.GOLD + ">"
-					+ChatColor.RESET + "" + ChatColor.RED  + "[ADMIN] " + ChatColor.RESET + "" + ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("PlayerSettings." + player.getDisplayName() + ".chatcolor")) + msg);
+					+ChatColor.RESET + "" + ChatColor.RED  + "[ADMIN]" + ChatColor.RESET + faction + ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("PlayerSettings." + player.getDisplayName() + ".chatcolor")) + msg);
 		}
 		
 	}
