bf0f0a4b44e14f80a0eb48565795bbda1cb05e1a
Sshawarma <sammytheshawarma@gmail.com>
2020-06-05 02:34:27 -0400

    Saving to avoid future sync issues

--- SMP/src/me/Sshawarma/Command/Messaging.java
+++ SMP/src/me/Sshawarma/Command/Messaging.java
@@ -3,12 +3,22 @@ package me.Sshawarma.Command;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class Messaging implements CommandExecutor{
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		// Messaging system in the works I suppose.
+		if(sender instanceof Player) {
+			
+			// MSG
+			if(cmd.getName().equalsIgnoreCase("msg")) {
+				
+			}
+			
+		}
+		
 		return false;
 	}
 
