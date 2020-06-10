8976864fe5b0324356858e9f7244e6aa2ca3f81d
Sshawarma <sammytheshawarma@gmail.com>
2020-06-09 22:05:21 -0400

    Player initialization still broken

--- SMP/src/me/Sshawarma/Events/JoinListener.java
+++ SMP/src/me/Sshawarma/Events/JoinListener.java
@@ -66,7 +66,7 @@ public class JoinListener implements Listener{
 	
 	@EventHandler
 	public void setFactionSpawn(PlayerRespawnEvent event) {
-		if(!event.isBedSpawn() && !plugin.getConfig().getString(event.getPlayer().getName() + ".faction").equals("cousins")) {
+		if(!event.isBedSpawn() && !plugin.getConfig().getString("PlayerSettings." + event.getPlayer().getName() + ".faction").equals("cousins")) {
 			Location spawn = event.getRespawnLocation().add(2406, 0, -697);
 			spawn.setY(event.getPlayer().getWorld().getHighestBlockYAt(spawn));
 			event.getPlayer().teleport(spawn);
