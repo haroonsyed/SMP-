eefd74e6f3a470b9575b492bf4717d86af9ed6b0
Sshawarma <sammytheshawarma@gmail.com>
2020-06-24 04:32:17 -0400

    Players should now be able to destory blocks of warring factions.
    -Performance update may be necessary.
    -Instead of player command, learn GP API in listener.

--- SMP/src/me/Sshawarma/Events/WarListener.java
+++ SMP/src/me/Sshawarma/Events/WarListener.java
@@ -46,14 +46,16 @@ public class WarListener implements Listener{
 		String ownerName = GriefPrevention.instance.dataStore.getClaimAt(player.getLocation(), true, null).getOwnerName();
 		UUID owner = Bukkit.getOfflinePlayer(ownerName).getUniqueId();
 		boolean isInWarringTerritory = config.getBoolean("FactionSettings." + config.getString("PlayerSettings." + owner.toString() + ".faction") + ".war.isWarring");
+		boolean ignoringClaims = GriefPrevention.instance.dataStore.getPlayerData(player.getUniqueId()).ignoreClaims;
 		
-		//USE FROM AND TO
-		if(isInWarringTerritory && isWarringFaction && haveAccess == false) {
+		
+		//USE FROM AND TO to track when the enter.
+		if(isInWarringTerritory && isWarringFaction && haveAccess == false && ignoringClaims == false) {
 			player.sendMessage("You are in warring faction's territory" + " OWNER: " + Bukkit.getOfflinePlayer(owner).getName());
 			Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "lp user " + player.getDisplayName() + " permission set griefprevention.ignoreclaims true");
 			player.performCommand("ignoreclaims");
 		}
-		else if(isWarringFaction) {
+		else if(isWarringFaction && ignoringClaims == true) {
 			//Only runs once because he wont have permission after
 			player.performCommand("ignoreclaims");
 			Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "lp user " + player.getDisplayName() + " permission set griefprevention.ignoreclaims false");
