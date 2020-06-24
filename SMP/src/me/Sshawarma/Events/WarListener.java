1248de80a2fe45a09796ec60537891a1bd7dbdf5
Sshawarma <sammytheshawarma@gmail.com>
2020-06-24 05:11:56 -0400

    Trying to fix NPE causing infinite ignoreclaim commands.

--- SMP/src/me/Sshawarma/Events/WarListener.java
+++ SMP/src/me/Sshawarma/Events/WarListener.java
@@ -31,34 +31,42 @@ public class WarListener implements Listener{
 		Plugin plugin = Main.getPlugin(Main.class);
 		FileConfiguration config = plugin.getConfig();
 		boolean isWarringFaction = config.getBoolean("FactionSettings." + config.getString("PlayerSettings." + event.getPlayer().getUniqueId().toString() + ".faction") + ".war.isWarring");
+		boolean ignoringClaims = GriefPrevention.instance.dataStore.getPlayerData(player.getUniqueId()).ignoreClaims;
 		
 		//Check if warmode is on
 		if(!isWarringFaction) {
 			return;
 		}
 		
-		boolean haveAccess = false;
-		
-		if(GriefPrevention.instance.dataStore.getClaimAt(player.getLocation(), true, null).allowAccess(player) == null) {
-			haveAccess = true;
+		//Check if player is not in a claim
+		if(GriefPrevention.instance.dataStore.getClaimAt(player.getLocation(), true, null) == null) {
+			
+			if(isWarringFaction && ignoringClaims == true) {
+				//Only runs once because he wont have permission after
+				player.performCommand("ignoreclaims");
+				Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "lp user " + player.getDisplayName() + " permission set griefprevention.ignoreclaims false");
+			}
+			
+			return;
 		}
+		
+		
 		
 		String ownerName = GriefPrevention.instance.dataStore.getClaimAt(player.getLocation(), true, null).getOwnerName();
 		UUID owner = Bukkit.getOfflinePlayer(ownerName).getUniqueId();
 		boolean isInWarringTerritory = config.getBoolean("FactionSettings." + config.getString("PlayerSettings." + owner.toString() + ".faction") + ".war.isWarring");
-		boolean ignoringClaims = GriefPrevention.instance.dataStore.getPlayerData(player.getUniqueId()).ignoreClaims;
+		boolean haveAccess = false;
 		
+		//Error here, fix
+		if(GriefPrevention.instance.dataStore.getClaimAt(player.getLocation(), true, null).allowAccess(player) == null) {
+					haveAccess = true;
+		}
 		
 		//USE FROM AND TO to track when the enter.
 		if(isInWarringTerritory && isWarringFaction && haveAccess == false && ignoringClaims == false) {
 			player.sendMessage("You are in warring faction's territory" + " OWNER: " + Bukkit.getOfflinePlayer(owner).getName());
 			Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "lp user " + player.getDisplayName() + " permission set griefprevention.ignoreclaims true");
 			player.performCommand("ignoreclaims");
-		}
-		else if(isWarringFaction && ignoringClaims == true) {
-			//Only runs once because he wont have permission after
-			player.performCommand("ignoreclaims");
-			Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(), "lp user " + player.getDisplayName() + " permission set griefprevention.ignoreclaims false");
 		}
 	}
 	
